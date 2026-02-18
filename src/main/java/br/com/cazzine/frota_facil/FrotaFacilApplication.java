fbba70d65774846bc248f4f487a5c8bd37ee89f7
Gustavo Cazzine <cazzinengustavo@gmail.com>
2026-02-18 19:34:35 -0300

    commit inicial:estrutura do projeto

---
+++ src/main/java/br/com/cazzine/frota_facil/FrotaFacilApplication.java
@@ -0,0 +1,13 @@
+package br.com.cazzine.frota_facil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrotaFacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrotaFacilApplication.class, args);
+	}
+
+}
